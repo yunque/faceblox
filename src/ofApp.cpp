94e3e35dc92598f0f591cf9b67ec15c7b56e5dd3
yunque <i.martinez08@alumni.imperial.ac.uk>
2017-10-29 21:47:24 +0000

    Find which tile was clicked
    Also tried to draw all tiles except one, but not quite there yet.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -38,11 +38,18 @@ void ofApp::draw(){
   random_shuffle(&hshuf[0], &hshuf[res]);
   
   // Draw the tiles
+  //int i = 0;
+//  while (i < res-1) {
   for (int w=0; w<res; w++){
     for (int h=0; h<res; h++){
+      //i += 1;
       int x = h;
       int y = w;
-      img.drawSubsection(widths[w], heights[h], sw,sh, wshuf[x], hshuf[y] );
+      //if (i < res-2){
+        img.drawSubsection(widths[w], heights[h], sw,sh, wshuf[x], hshuf[y] );
+      //}
+
+      //img.crop(widths[x], heights[h], sw,sh));
       // NB. drawSubsection params...
       /// \param x X position to draw cropped image at.
       /// \param y Y position to draw cropped image at.
@@ -52,10 +59,20 @@ void ofApp::draw(){
       /// \param sy Y position in image to begin cropping from.
    }
   }
+//  }
+  ofColor c  = img.getColor(iw/2, ih/2);
+  //cout << c << endl;
+  //count << &c[0] << endl;
 
   // Chill!
   sleep(1);
 }
+
+/*
+void blurPixels(int x0, y0, x1, y1){
+  // Blur pixels between (x0,y0) & (x1,y1) to avg. color inside region
+}
+*/
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
@@ -79,7 +96,22 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+  int res = 3;
+  int iw = img.getWidth();
+  int ih = img.getHeight();
+  int col, row, r;
 
+  // Find which tile was selected
+  for (r=0; r<res; r++){
+    if ((r*iw/res < mouseX) && (mouseX < (r+1)*iw/(res))) {
+      col = r;
+      printf("col = %i\n", r);
+    }
+    if ((r*ih/res < mouseY) && (mouseY < (r+1)*ih/(res))) {
+      row = r;
+      printf("row = %i\n", r);
+    }
+  }
 }
 
 //--------------------------------------------------------------
